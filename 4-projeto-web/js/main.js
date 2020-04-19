3d4399dc4bc8c49f0800c2fdf4b2c80c4ef8327b
Daniel Baia <danielbaia29@gmail.com>
2020-04-19 20:22:45 -0300

    create project

---
+++ 4-projeto-web/js/main.js
@@ -0,0 +1,97 @@
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+});
+
+$(".rslides_portfolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true
+});
+
+Visibility.onVisible(function() {
+    setTimeout(function(){
+      $(".introduction h1").addClass("animated fadeInDown");
+    }, 400);
+    
+    setTimeout(function(){
+      $(".introduction blockquote").addClass("animated fadeInDown");
+    }, 800);
+    
+    setTimeout(function(){
+      $(".btn").addClass("animated fadeInDown");
+    }, 1200);
+      
+    setTimeout(function(){
+      $(".animar").addClass("animated fadeInDown");
+    }, 1600);
+    
+    
+    setTimeout(function(){
+      $(".introduction-interna h1").addClass("animated fadeInDown");
+    }, 400);
+    
+    setTimeout(function(){
+      $(".introduction-interna p").addClass("animated fadeInDown");
+    }, 800);
+    setTimeout(function(){
+      $(".animar-interna").addClass("animated fadeInDown");
+    }, 1600);
+});
+
+/*Formulario de Contato*/
+// Formulario
+
+$('.formphp').on('submit', function() {
+	var emailContato = "elementartec@gmail.com"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+			
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
